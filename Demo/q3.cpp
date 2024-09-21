7e338c26eda02fb3cfc611f63a9c042190546d22
dannywhitley <dzw0064@auburn.edu>
2024-09-20 20:18:26 -0500

    Update q3.cpp

--- Demo/q3.cpp
+++ Demo/q3.cpp
@@ -1,26 +1,61 @@
-#include <iostream>;
-#include <iomanip>;
-
+#include <iostream>
 using namespace std;
 
-double convertToMPH(int minutes, int seconds);
-double convertToMPH(double kph);
+// Function declarations
+double convertToMPH(int minutes, int seconds); // User enters the time it takes them to run 1 mile and the rate is determined
+double convertToMPH(double kilometersPerHour); // User enters their running rate in KPH and it is converted to MPH
 
-int main() {
-	int minutes = 0,
-	    seconds = 0;
-	double kph = 0.0;
+int main()
+{
+    // Declares input variables
+    int inputMinutes, inputSeconds;
+    double inputKPH;
 
+    // Displays messages prompting the user to input values
+    cout << "Enter the time spent running one mile in minutes and seconds: \n" << "Minutes: ";
+    cin >> inputMinutes; 
+    cout << "Seconds: ";
+    cin >> inputSeconds;
+    cout << "Enter a running pace in kilometers per hour: "; 
+    cin >> inputKPH;
 
+    // Sets the precision to 2 decimal points, see pg 31-32
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
 
-	return 0;
+    // Caculates the running rate in MPH from the first definition of convertToMPH
+    cout << "The running rate in MPH based on the time entered (First definition of convertToMPH) is: ";
+    cout << convertToMPH(inputMinutes, inputSeconds) << " MPH." << endl;
+
+    // Calculates the running rate in MPH from the second definition of convertToMPH
+    cout << "The running rate in MPH based on the rate in KPH entered (Second definition of convertToMPH) is: ";
+    cout << convertToMPH(inputKPH) << " MPH." << endl;
 }
 
-double convertToMPH(int minutes, int seconds) {
-	// There are 3600 seconds in an hour
-	return (3600.0 / ((minutes * 60) + seconds));
+// First definition of convertToMPH using minutes and seconds 
+double convertToMPH(int minutes, int seconds)
+{
+    // Declares the hours spent running 1 mile and MPH
+    double hours;
+    double mph;
+
+    // Converts the minutes and seconds to hours
+    hours = ((minutes / 60.0) + (seconds / 3600.0));
+
+    // Calculates the mph
+    mph = 1 / hours;
+
+    // Returns a value for the runners speed in MPH and type double
+    return(mph);
 }
 
-double convertToMPH(double kph) {
-	return (kph / 1.61);
+// Second definition using kilometers per hour
+double convertToMPH(double kilometersPerHour)
+{
+    // Declares a constant variable of type double for the conversion factor KPH to MPH
+    const double KMPERMILE = 1.61;
+
+    // Returns a value of MPH when the function is called 
+    return(kilometersPerHour / KMPERMILE);
 }
